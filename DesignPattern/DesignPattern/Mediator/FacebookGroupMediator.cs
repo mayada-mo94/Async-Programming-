36c5b2be00617bd78105a187327520c45e88c6d1
mayada94 <mmohsen@integrant.com>
2021-09-13 18:40:35 +0200

    initial commit

---
+++ DesignPattern/DesignPattern/Mediator/FacebookGroupMediator.cs
@@ -0,0 +1,8 @@
+﻿namespace DesignPattern.Mediator
+{
+    public interface FacebookGroupMediator
+    {
+        void SendMessage(string msg, User user);
+        void RegisterUser(User user);
+    }
+}
